e921b8ec44275a787e95fc4e9e87dd8731e445b6
Carlos Gimeno Yáñez <cgimeno@bifi.es>
2014-07-22 11:45:27 +0200

    Added Salvatore Pinto pushes to ChangeLog. gpvcmupdate 0.0.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
             use_setuptools()
             from setuptools import setup, find_packages
             
-version = "0.0.2"
+version = "0.0.3"
 productname = "gpvcmupdate"
 
 
